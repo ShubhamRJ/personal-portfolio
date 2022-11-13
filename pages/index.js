dce4b644978f89e10290849a187ed44c9ff0ffef
Shubham <shubhamjawandhiya304@gmail.com>
2022-11-13 14:32:05 -0500

    fix: [refactor] edit content

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import homeStyles from './index.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import homeStyles from "./index.module.css";
 
 const subTitle = "Software Engineer";
 
@@ -12,40 +12,75 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <header>
-        <h1 className={`${utilStyles.headingLg} ${homeStyles.title} ${homeStyles.headings}`}>{siteTitle}</h1>
+        <h1
+          className={`${utilStyles.headingLg} ${homeStyles.title} ${homeStyles.headings}`}
+        >
+          {siteTitle}
+        </h1>
         <h2 className={homeStyles.subtitle}>
           {subTitle} | Currently @
-          <a href='https://lowes.co.in/' target="_blank">Lowe's India</a>
+          <a href="https://lowes.co.in/" target="_blank">
+            Lowe's India
+          </a>
         </h2>
       </header>
       <div className={homeStyles.mediaLinks}>
-        <a href='https://www.linkedin.com/in/shubhamjawandhiya/' target='_blank'>
-          <img src='/images/linkedin.svg' height={25}  className={homeStyles.mediaImg} />
+        <a
+          href="https://www.linkedin.com/in/shubhamjawandhiya/"
+          target="_blank"
+        >
+          <img
+            src="/images/linkedin.svg"
+            height={25}
+            className={homeStyles.mediaImg}
+          />
         </a>
-        <a href='https://github.com/ShubhamRJ' target='_blank'>
-          <img src='/images/github.svg' height={25}  className={homeStyles.mediaImg} />
+        <a href="https://github.com/ShubhamRJ" target="_blank">
+          <img
+            src="/images/github.svg"
+            height={25}
+            className={homeStyles.mediaImg}
+          />
         </a>
-        <a href='https://twitter.com/ShubhamRJ28' target='_blank'>
-          <img src='/images/twitter.svg' height={25}  className={homeStyles.mediaImg} />
+        <a href="https://twitter.com/ShubhamRJ28" target="_blank">
+          <img
+            src="/images/twitter.svg"
+            height={25}
+            className={homeStyles.mediaImg}
+          />
         </a>
-        <div title='Mail to: shubhamjawandhiya304@gmail.com'>
-          <img src='/images/gmail.svg' height={25}  className={homeStyles.mediaImg} />
+        <div title="Mail to: shubhamjawandhiya304@gmail.com">
+          <img
+            src="/images/gmail.svg"
+            height={25}
+            className={homeStyles.mediaImg}
+          />
         </div>
-        <a href='https://stackoverflow.com/users/8352271/shubham-jawandhiya' target='_blank'>
-          <img src='/images/stack-overflow.svg' height={25}  className={homeStyles.mediaImg} />
+        <a
+          href="https://stackoverflow.com/users/8352271/shubham-jawandhiya"
+          target="_blank"
+        >
+          <img
+            src="/images/stack-overflow.svg"
+            height={25}
+            className={homeStyles.mediaImg}
+          />
         </a>
       </div>
       <div>
-        <h2 className={`${homeStyles.headings} ${homeStyles.aboutMe}`}>About Me</h2>
-        <p className={homeStyles.subtitle} style={{fontWeight: '200'}}>
-          Hi! I'am Shubham, a 23 y/o Software Engineer. I love to solve problems and build products & experiences without limiting myself to a particular tech stack.
-          Currently exploring Frontend Engineering at Lowe's India.
-          Looking forward to sharing my knowledge via blog articles, conferences etc.
+        <h2 className={`${homeStyles.headings} ${homeStyles.aboutMe}`}>
+          About Me
+        </h2>
+        <p className={homeStyles.subtitle} style={{ fontWeight: "200" }}>
+          Hi! I'am Shubham, a 24 y/o Software Engineer. I love to solve complex
+          problems and build products & experiences. Currently completing my
+          M.S. in Computer Science from Stony Brook University, NY. Looking
+          forward to sharing my knowledge via blog articles, conferences etc.
           <br />
           <br />
           Feel free to reach out and say Hi!
         </p>
       </div>
     </Layout>
-  )
+  );
 }
