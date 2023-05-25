82f950ab9a23e1da037cc0c2171d75caf3c15b9d
Shubham <shubhamjawandhiya304@gmail.com>
2023-05-25 13:34:37 -0400

    minor updates

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -10,6 +10,8 @@ export default function About() {
       <GradientText h2 size={device == "lg" ? "$6xl" : "4xl"} text="About Me" />
       <br />
       <Text h4 css={{ color: "$gray700" }} weight="normal" size="$lg">
+        Curious | Ideator | Perpetual Learner
+        <br />
         Hey there! I am junior software engineer based in NY, United States.
         Currently a graduate student at{" "}
         <Link href="https://www.stonybrook.edu/" target="_blank">
@@ -20,13 +22,7 @@ export default function About() {
           ></GradientText>
         </Link>
         <br />
-        Generalist software engineer experimenting and finding my niche.
-        <br />
-        Extremely curious by nature, loves to explore new products and ideas.
-        Continuously learning something new.
-        <br />
-        My main focus currently is in Natural Language Processing research and
-        learning Distributed Systems.
+        I am continuously working on creating, evaluating and writing new ideas on my personal blog. I have a special knack for understanding the products & services from a customer's perspective and orienting my ideas and code accordingly. I lookout for challenges and learning opportunities from engineers around me.
         <br />
         I have experience with:
         <br />{" "}
@@ -39,7 +35,7 @@ export default function About() {
           size="$lg"
         />
         <Text h5 css={{ color: "$gray700" }} weight="normal">
-          Python3, JavaScript, TypeScript, C, C++, Golang
+          Python3, JavaScript, TypeScript, C, C++, SQL
         </Text>
       </div>
       <div className={styles.listdiv}>
@@ -73,8 +69,8 @@ export default function About() {
           size="$lg"
         />
         <Text h5 css={{ color: "$gray700" }} weight="normal">
-          Natural Language Processing, Deep Learning, Data Structures and
-          Algorithms, Evolutionary Algorithms
+          Natural Language Processing, Data Structures and
+          Algorithms, Evolutionary Algorithms, Advanced OS, Databases
         </Text>
       </div>
       {/* {(device == "lg" || device == "md") && <br />}
